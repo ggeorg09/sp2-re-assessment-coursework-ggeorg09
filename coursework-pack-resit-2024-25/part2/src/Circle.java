67bd4e85e3f3eb8762b8a98cf5bb0704e33cbc19
G. Georgiev <georgirgeorgiev89@gmail.com>
2025-08-14 20:55:36 +0300

    Declaring the instance variable radius which is the radius of the circle

---
+++ coursework-pack-resit-2024-25/part2/src/Circle.java
@@ -0,0 +1,10 @@
+/**
+ * Circle is GeometricObject that has a radius and have scalable interface to allow scaling the circle size.
+ * 
+ */
+
+public class Circle extends GeometricObject implements Scalable { 
+// The radius of the circle
+ private double radius;
+
+ 
