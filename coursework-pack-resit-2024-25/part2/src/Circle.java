e07b3596b834bc544e3160e7685f6bf6d23491d3
G. Georgiev <georgirgeorgiev89@gmail.com>
2025-08-14 21:02:12 +0300

    Creating the circle with the radius, value and color

--- coursework-pack-resit-2024-25/part2/src/Circle.java
+++ coursework-pack-resit-2024-25/part2/src/Circle.java
@@ -13,5 +13,30 @@ public class Circle extends GeometricObject implements Scalable {
     public Circle() { 
         this(1.0); 
     }
+     /**
+     * creating the circle with the radius
+     * @param radius radius of the circle
+     */
+    public Circle(double radius) { 
+        this.radius = radius; 
+    }
+
+    /**
+     * Creating the circle with specified radius, color, and value
+     * @param radius radius of the circle
+     * @param color color of the circle
+     * @param filled whether the circle is filled
+     */
+    public Circle(double radius, String color, boolean filled) {
+        super(color, filled);
+        this.radius = radius;
+    }
+
+    /**
+     * @return the radius of the circle
+     */
+    public double getRadius() { 
+        return radius; 
+    }
 
     /**
