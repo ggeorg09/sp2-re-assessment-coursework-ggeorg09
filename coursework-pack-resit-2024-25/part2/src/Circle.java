420b8196562575fd77fe138ca4b186ab1fb72f29
G. Georgiev <georgirgeorgiev89@gmail.com>
2025-08-14 20:57:05 +0300

    Creating the circle to be with radius 1.0

--- coursework-pack-resit-2024-25/part2/src/Circle.java
+++ coursework-pack-resit-2024-25/part2/src/Circle.java
@@ -7,4 +7,11 @@ public class Circle extends GeometricObject implements Scalable {
 // The radius of the circle
  private double radius;
 
- 
+    /**
+     * Creating the circle to be with radius 1.0
+     */
+    public Circle() { 
+        this(1.0); 
+    }
+
+    /**
